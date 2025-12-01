5f6774872e9a111d27d9e9e7fd3fc7292d2b55a2
ebbajoh <ebbajohansson999@gmail.com>
2025-12-01 11:54:53 +0100

    ny fil för att hämta kort loggorna

---
+++ cardLogos.js
@@ -0,0 +1,11 @@
+export const cardLogos = {
+  visa: require("./assets/cards/visa.png"),
+  mastercard: require("./assets/cards/mastercard.png"),
+  amex: require("./assets/cards/amex.png"),
+  discover: require("./assets/cards/discover.png"),
+  dinersclub: require("./assets/cards/dinersclub.png"),
+  jcb: require("./assets/cards/jcb.png"),
+  troy: require("./assets/cards/troy.png"),
+  unionpay: require("./assets/cards/unionpay.png"),
+  unknown: require("./assets/cards/chip.png"), // fallback
+};
